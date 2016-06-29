c890bf5a5f7a5a36a44241c27dfd66ea30ea1868
Martha Fairbanks <marthafairbanks@Marthas-MBP.T-mobile.com>
2016-06-28 23:21:39 -0400

    validation token at login

--- js/adminLoginTest.js
+++ js/adminLoginTest.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
 	$('input.form-control.username').focus();
 	// localStorage.removeItem("token");
+	console.log(localStorage);
 
-	if (localStorage.token === '{"token":"M44ASR0FL0PJH3OLJ5RC"}') {
-		window.location.replace('admin.html');
-	}
-
-	else {
-	
-	
-
-	$('form').on("submit",function(event){
+	function checkToken() {
+		if (localStorage.token === M44ASR0FL0PJH3OLJ5RC) {
+			console.log("yay you been here before!");
+			// window.location.replace('admin.html');
+		}
+		else
+			console.log("please enter your username");
+			$('form').on("submit",function(event){
 		event.preventDefault();
 
 		var username = $(this).find('input.form-control.username').val();
@@ -33,20 +33,20 @@ $( document ).ready(function() {
         data:data,
         success: function(result){
         	if ((JSON.stringify(result)) === '{"error":"Your login information was not right :("}'){
-        		alert("Your username or password is incorrect.")
+        		alert("Your username or password is incorrect.");
         	}
         	else {
 		        console.log((JSON.stringify(result)));
 		        var token = (JSON.stringify(result));
-		        localStorage.setItem("token", token);
-	        	window.location.replace('admin.html');
+		        // localStorage.setItem("token", token);
+		        // console.log(localStorage);
+	        	// window.location.replace('admin.html');
 	        }	
     }});
 
 
 
 	});
-
 	}
 
 });	

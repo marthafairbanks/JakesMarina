feb2dc8da73a4cb8dfba54fb5a5e872212198311
Malorie Kramer <maloriekramer@malorieamersmbp.attlocal.net>
2016-07-03 09:25:19 -0400

    friday stuff

--- js/icons.js
+++ js/icons.js
@@ -1,24 +1,20 @@
 loadWeather('Brookville,IN');
 
 
-
-
 function loadWeather(location) {
 	$.simpleWeather({
 		location: location,
 		unit:'f',
 		success: function(weather) {
-			city = weather.city;
 			temp = weather.temp+'&deg;';
 			wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
 			wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
 			humidity = weather.humidity + ' %';
 
-			$(".location").text(city);
-			$(".temperature").html(temp);
-			$(".climate_bg").html(wcode);
-			$(".windspeed").html(wind);
-			$(".humidity").text(humidity);
+		
+			$(".tempIcon").html(temp);
+			$(".weatherImg").html(wcode);
+		
 		},
 		error: function(error) {
 			$(".error").html('<p>' + error + '</p>');
